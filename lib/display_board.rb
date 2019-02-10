ba28f739629de141f385ff2049ad2eed14d7d2ab
Moufoulyroun Kouferidji <micromouf@yahoo.fr>
2019-02-10 17:19:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,11 @@
 def display_board
   puts "a 3x3 tic tac toe board"
   
-  
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end  
   
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
